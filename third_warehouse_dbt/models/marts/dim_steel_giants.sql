8b72223b7a0ce9a77a877b7d8953dd08f385e84d
Likith <likithsai26@icloud.com>
2026-04-19 16:18:17 +0530

    Create dim_steel_giants mart table

---
+++ third_warehouse_dbt/models/marts/dim_steel_giants.sql
@@ -0,0 +1,27 @@
+{{ config(schema='marts') }}
+{{ config(materialized='table') }}
+
+with aggregations AS (
+select 
+    parent_name, 
+    parent_type, 
+    sum(weighted_emissions_co2e) as total_co2e_impact_tonnes,
+from {{ ref('fct_steel_emissions_attribution') }}
+group by 1, 2),
+
+ranking as (
+    select
+        *,
+        dense_rank() over (
+            partition by parent_type 
+            order by total_co2e_impact_tonnes desc
+        ) as category_rank,
+
+        dense_rank() over (
+            order by total_co2e_impact_tonnes desc
+        ) as global_rank
+    from aggregations
+)
+
+select * from ranking
+ORDER BY parent_type ASC, category_rank ASC
